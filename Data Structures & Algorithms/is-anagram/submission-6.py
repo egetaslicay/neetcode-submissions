11ca2af281241c6a830df69de3fb2b371b7bd9f4
Ege Taslicay <90542457+egetaslicay@users.noreply.github.com>
2026-07-05 16:12:15 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): 
+            return False
+
+        countert = Counter(t)
+        counters = Counter(s)
+
+        for char in countert: 
+            if countert[char] != counters[char]: 
+                return False
+
+        return True
