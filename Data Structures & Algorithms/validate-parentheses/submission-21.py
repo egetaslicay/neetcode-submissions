fbb78ffa0a91f0009ccf894a196df5df5331daf2
Ege Taslicay <90542457+egetaslicay@users.noreply.github.com>
2026-07-16 22:14:36 -0700

    Add: validate-parentheses - submission-21

---
+++ Data Structures & Algorithms/validate-parentheses/submission-21.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # set hashMap
+        closeToOpen = {} 
+        closeToOpen[']'] = '['
+        closeToOpen['}'] = '{'
+        closeToOpen[')'] = '('
+
+        stack = []
+
+        for char in s: 
+            if char in closeToOpen: 
+                if stack and stack[-1] == closeToOpen[char]: 
+                    stack.pop()
+                else: 
+                    return False
+            else: 
+                stack.append(char)
+
+        if stack: 
+            return False
+
+        return True
+        
+
+            
+                
+
+
+
+    
