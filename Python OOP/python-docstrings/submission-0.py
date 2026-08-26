94833519472bd77cbefde754540f49f99ea1c3cf
Ege Taslicay <egetaslicay@gmail.com>
2026-08-25 23:37:57 -0700

    Add: python-docstrings - submission-0

---
+++ Python OOP/python-docstrings/submission-0.py
@@ -0,0 +1,29 @@
+class Pet:
+    """
+    A class to represent a pet 
+
+    Attributes: 
+    name (str): Pet's name
+    animal_type (str): type of animal the pet is 
+
+    """ 
+
+
+    def __init__(self, name: str, animal_type: str):
+        """ Initialize a new pet instance """ 
+        self.name = name
+        self.animal_type = animal_type
+
+    def make_sound(self) -> str:
+        if self.animal_type == "dog":
+            return "Woof!"
+        elif self.animal_type == "cat":
+            return "Meow!"
+        else:
+            return "Unknown sound"
+
+# Don't change the following code
+print(Pet.__doc__)
+print(Pet.__init__.__doc__)
+print(Pet.make_sound.__doc__)
+
